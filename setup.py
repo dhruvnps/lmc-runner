20371d83c3bcc5eb024a99bf2884a50749849449
dhruvpattem <dhruv.pattem@gmail.com>
2020-11-16 18:25:59 +0000

    updates

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lmc-runner",
-    version="0.0.1",
+    version="0.0.2",
     author="dhruvnps",
     description="Run LMC assembly code!",
     long_description=long_description,
